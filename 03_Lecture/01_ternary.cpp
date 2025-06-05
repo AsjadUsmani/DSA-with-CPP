707eb5dc917bb371dc71c6d4bdc347cfc998e6b7
Asjad Usmani <asjadusmani4@gmail.com>
2025-06-05 21:50:20 +0530

    Saved my local changes before rebase

--- 03_Lecture/01_ternary.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    int a = -12;
-    a >= 0 ? cout << a << " is Positive" << endl : cout << a << " is Negative." << endl;
-    return 0;
-}
