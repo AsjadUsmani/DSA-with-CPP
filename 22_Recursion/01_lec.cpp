3a4d1376c004b869192f2f7424657958a9d338cf
Asjad Usmani <asjadusmani4@gmail.com>
2025-08-05 21:47:11 +0530

    Introduction to Recursion

---
+++ 22_Recursion/01_lec.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void printNum(int n) {
+    if(n == 1){
+        cout << 1;
+        return;
+    }
+    cout << n << " ";
+    printNum(n - 1);
+}
+
+int fact(int n){
+    if(n == 1){
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+int sumOfNNumbers(int n){ // TC: O(n), SC: O(n)
+    if(n == 0) return 0;
+    return n + sumOfNNumbers(n - 1);
+}
+
+int main(){
+    printNum(5);
+    cout << endl;
+    cout << fact(4) << endl;
+    cout << sumOfNNumbers(4) << endl;
+    return 0;
+}
+
+// TC = Total calls in recursion tree * work done in each call
+// SC = Depth of Rec. tree (Height of call stack) * auxillary space occupied
